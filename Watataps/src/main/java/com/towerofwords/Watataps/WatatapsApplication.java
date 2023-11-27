087c90639bedef0433308e5171cea130e10e7d79
Rey Dante Garcia <116933977+satou0419@users.noreply.github.com>
2023-11-27 11:04:37 +0800

    v0 --- Revertable User/UserDetails/Item

---
+++ Watataps/src/main/java/com/towerofwords/Watataps/WatatapsApplication.java
@@ -0,0 +1,13 @@
+package com.towerofwords.Watataps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatatapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatatapsApplication.class, args);
+	}
+
+}
---
+++ Watataps/src/test/java/com/towerofwords/Watataps/WatatapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.towerofwords.Watataps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatatapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
